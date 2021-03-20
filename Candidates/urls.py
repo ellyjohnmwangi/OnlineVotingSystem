36ccb697c258d123302baa1761d3170d4fae7991
ellyjohnmwangi <johnmwangielly@gmail.com>
2021-03-20 14:27:34 +0300

    login/logout urls added

--- Candidates/urls.py
+++ Candidates/urls.py
@@ -4,4 +4,6 @@ from .views import *
 urlpatterns = [
     path('', IndexView.as_view(), name="index"),
     path('login/', UserLoginView.as_view(), name="login"),
+    path('logout/', UserLoginView.as_view(), name="logout"),
+    path('register/', UserLoginView.as_view(), name="register"),
     ]
