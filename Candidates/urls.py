6b0b60d9ef2a9c90d6fd3690b9e8243d9de99179
ellyjohnmwangi <johnmwangielly@gmail.com>
2021-04-02 11:19:49 +0300

    login/logout urls syntax fix

--- Candidates/urls.py
+++ Candidates/urls.py
@@ -4,6 +4,6 @@ from .views import *
 urlpatterns = [
     path('', IndexView.as_view(), name="index"),
     path('login/', UserLoginView.as_view(), name="login"),
-    path('logout/', UserLoginView.as_view(), name="logout"),
-    path('register/', UserLoginView.as_view(), name="register"),
+    path('logout/', LogoutView.as_view(), name="logout"),
+    path('register/', RegisterView.as_view(), name="register"),
     ]
