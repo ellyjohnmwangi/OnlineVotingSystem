e927918819cf86e632dbcd4934dd4ef671708263
ellyjohnmwangi <johnmwangielly@gmail.com>
2021-04-04 01:09:47 +0300

    Registered Candidate and Position models

--- Candidates/admin.py
+++ Candidates/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Candidate, Position
+
 
 # Register your models here.
+# @admin.register(Candidate)
+class AdminCandidate(admin.ModelAdmin):
+    readonly_fields = ['votes']
+
+    class Meta:
+        model = Candidate
+
+
+admin.site.register(Candidate, AdminCandidate)
+admin.site.register(Position)
