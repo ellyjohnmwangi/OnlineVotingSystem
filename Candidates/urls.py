80b3c18563f29a1222364e7f490a16873c52ebf6
ellyjohnmwangi <johnmwangielly@gmail.com>
2021-04-04 12:18:34 +0300

    Candidate view url added

--- Candidates/urls.py
+++ Candidates/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('login/', UserLoginView.as_view(), name="login"),
     path('logout/', LogoutView.as_view(), name="logout"),
     path('register/', RegisterView.as_view(), name="register"),
+    path('vote/', CandidatesView.as_view(), name="vote"),
     ]
